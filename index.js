3ba1689ab819a0133b3327ad6237ac5998c6f99c
Ajit Mehrotra <ajitmehrotra2009@gmail.com>
2020-04-18 18:06:14 -0400

    Add promises and added coloring to terminal output

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 #!/usr/bin/env node
 const fs = require("fs");
+const { lstat } = fs.promises;
+const chalk = require("chalk");
 
-fs.readdir(process.cwd(), (err, filenames) => {
+fs.readdir(process.cwd(), async (err, filenames) => {
   if (err) {
     //error handling code here
     console.log(err);
   }
-  console.log(filenames);
+  const statPromises = filenames.map((filename) => {
+    return lstat(filename);
+  });
+
+  const allStats = await Promise.all(statPromises);
+
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
+
+    if (stats.isFile()) {
+      console.log(filenames[index]);
+    } else {
+      console.log(chalk.red(filenames[index]));
+    }
+  }
 });
